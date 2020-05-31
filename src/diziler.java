26872518545bfcf7ba10501f93a419ea6a4cdc87
eresh44 <66196686+eresh44@users.noreply.github.com>
2020-05-31 11:29:28 +0300

    no msg

---
+++ src/diziler.java
@@ -0,0 +1,26 @@
+
+public class diziler {
+
+
+		public static void main(String[] args) {
+			double sayý[][] = {
+				{ 1230, 300, 1469, 7654},
+				{400, 5000, 44, 6547},
+			    {9000,4697,4,63},
+				{234,6974,4,63},
+			};
+			
+			
+
+			for(int i=0 ; i<4 ; i++) {
+				for(int j=0 ; j<4 ; j++) {
+				
+				System.out.print( sayý[i][j] + "  ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
