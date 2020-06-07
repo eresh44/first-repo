0fe8f08720f24357e7ce3c380c58b9c46927eed9
eresh44 <66196686+eresh44@users.noreply.github.com>
2020-06-07 10:52:02 +0300

    sayı

---
+++ src/aritmetikoperatorler2.java
@@ -0,0 +1,16 @@
+
+public class aritmetikoperatorler2 {
+
+	public static void main(String[] args) {
+		 int sayı1, sayı2,sayı3,sayı4 ;
+		 sayı1= 444 ;
+		 sayı2=sayı1+ 6;
+		 sayı3= 222;
+		 sayı4= sayı1*sayı2;
+		 System.out.println("sonuc1: " + sayı1);
+		 System.out.println("sonuc2: " + sayı2);
+		 System.out.println("sonuc3: " + sayı3);
+		 System.out.println("sonuc4: " + sayı4);
+	}
+
+}
---
+++ src/modalma.java
@@ -0,0 +1,11 @@
+
+public class modalma {
+
+	public static void main(String[] args) {
+		int sayý1=30;
+		System.out.println(sayý1 + "sayýnýn mod 3 = " + sayý1 % 3);
+		sayý1=34;
+		System.out.println(sayý1 + "sayýnýn mod 3 = " + sayý1 % 3);
+	}
+
+}
