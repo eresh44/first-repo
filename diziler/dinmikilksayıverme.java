b3c8d7c9413d141bacbc905174a0219a8ab7b65c
eresh44 <66196686+eresh44@users.noreply.github.com>
2020-06-10 08:33:17 +0300

    d

---
+++ diziler/dinmikilksayıverme.java
@@ -0,0 +1,15 @@
+package eresh;
+
+public class dinmikilksayıverme {
+
+	public static void main(String[] args) {
+		int sayı1=12, sayı2=15;
+		double sayı3;
+		double a=20;
+		
+		sayı3= (float)Math.sin(sayı2)+a;
+		System.out.println(sayı3 *sayı2);
+
+	}
+
+}
