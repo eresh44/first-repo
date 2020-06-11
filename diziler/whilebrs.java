49e4fbad9f561002d76f517c048aff4e015fd465
eresh44 <66196686+eresh44@users.noreply.github.com>
2020-06-11 12:49:59 +0300

    b

---
+++ diziler/whilebrs.java
@@ -0,0 +1,13 @@
+
+public class whilebrs {
+
+	public static void main(String[] args) {
+	
+		int i=10;
+		int j=30;
+				while(++i < --j) {
+					System.out.println("i sayýsý" + i + " j sayýsý: " + j);
+				}
+	}
+
+}
