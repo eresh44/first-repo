b0dc6123e1dca769e845fe1bd131538b7250ed0c
eresh44 <66196686+eresh44@users.noreply.github.com>
2020-06-11 12:59:36 +0300

    do

---
+++ src/dowhile.java
@@ -0,0 +1,17 @@
+
+public class dowhile {
+
+	public static void main(String[] args) {
+	int sayý= 4;
+	do {
+		System.out.println("SONUC: " + sayý);
+		sayý++ ; }
+	
+	
+	
+	while (sayý < 8 );
+	
+
+	}
+
+}
